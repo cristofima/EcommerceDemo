151293089f092fbadfa184c93431e8d74a18cd78
Cristopher Coronado Moreira <cristofima@hotmail.com>
2020-10-13 22:47:58 -0500

    Init project

---
+++ ApiTasksDemo/Models/Note.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ApiTasksDemo.Models
+{
+    public partial class Note
+    {
+        [Key]
+        public int NoteId { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool IsCompleted { get; set; }
+        [Required]
+        [Range(1, 3)]
+        public int Priority { get; set; }
+        [Required]
+        public DateTime ExpirationDate { get; set; }
+    }
+}
---
+++ ApiTasksDemo/Models/TestContext.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ApiTasksDemo.Models
+{
+    public partial class TestContext : DbContext
+    {
+        public TestContext()
+        {
+        }
+
+        public TestContext(DbContextOptions<TestContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Note> Notes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseNpgsql("Host=localhost;Database=Test;Username=postgres;Password=12345678");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Note>(entity =>
+            {
+                entity.HasKey(e => e.NoteId)
+                    .HasName("notes_pkey");
+
+                entity.Property(e => e.NoteId).HasDefaultValueSql("nextval('notes_noteid_seq'::regclass)");
+
+                entity.Property(e => e.Description).HasMaxLength(200);
+
+                entity.Property(e => e.Title)
+                    .IsRequired()
+                    .HasMaxLength(30);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
